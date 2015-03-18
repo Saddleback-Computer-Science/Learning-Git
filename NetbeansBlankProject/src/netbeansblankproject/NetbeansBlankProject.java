a613f3796dc9369a6f4160827e1720395a4513ce
gitBC <bcrist1@saddleback.edu>
2015-03-18 09:38:33 -0700

    Adding blank project - New .gitignore for netbeans

---
+++ NetbeansBlankProject/src/netbeansblankproject/NetbeansBlankProject.java
@@ -0,0 +1,16 @@
+package netbeansblankproject;
+
+/**
+ *
+ * @author 
+ */
+public class NetbeansBlankProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
